d0f8398ffd2f2d843de3c18e6af0acb79a33a409
Settor Kofi Amediku <43499539+settor21@users.noreply.github.com>
2024-01-11 16:19:47 +0000

    Update app.py to listen to all ports
    if __name__ == '__main__':
        app.run(host = "0.0.0.0",debug=True,port= 5000) #will listen to all ports on production

--- app.py
+++ app.py
@@ -103,4 +103,4 @@ def signup():
     return render_template('signup.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,port= 5000)
+    app.run(host = "0.0.0.0",debug=True,port= 5000) #will listen to all ports on production
