ffaebf78aa5051620d016c87ad901d50dafea0c9
settor21 <43499539+settor21@users.noreply.github.com>
2024-01-16 05:18:07 +0000

    postgres app.py

--- app.py
+++ app.py
@@ -1,67 +1,182 @@
-from pymongo import MongoClient
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+import requests
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify, make_response
-import requests, uuid
+import uuid
+import psycopg2
+from bcrypt import hashpw, gensalt
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-DB_SERVICE_URL = 'http://127.0.0.1:6000'
+# PostgreSQL Configuration
+DB_HOST = '35.239.170.49'  # Replace with the IP address of your GCP VM
+DB_PORT = 5432
+DB_USER = 'amedikusettor'
+DB_PASSWORD = 'Skaq0084'
+DB_NAME = 'users'
 
 
-# Initialize MongoDB client
-mongo_client = MongoClient(
-    'mongodb+srv://amedikusettor:Skaq0084@myflixproject.soxjrzv.mongodb.net/?retryWrites=true&w=majority')
-db = mongo_client['userSubscriptions']
-subscription_collection = db['subscriptionInit']
+def create_tables():
+    with psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME
+    ) as connection:
+        cursor = connection.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                first_name TEXT NOT NULL,
+                last_name TEXT NOT NULL,
+                email TEXT NOT NULL UNIQUE,
+                password TEXT NOT NULL
+            )
+        ''')
+
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS subscriptions (
+                subscriptionId SERIAL PRIMARY KEY,
+                userId INTEGER,
+                paidSubscriber TEXT NOT NULL,
+                amount REAL NOT NULL,
+                FOREIGN KEY (userId) REFERENCES users (id)
+            )
+        ''')
+
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS sessions (
+                id SERIAL PRIMARY KEY,
+                user_id INTEGER,
+                session_id TEXT NOT NULL,
+                FOREIGN KEY (user_id) REFERENCES users (id)
+            )
+        ''')
+
+        connection.commit()
+
 
 # Function to check if a user with given email and password exists in db
 def authenticate_user(email, password):
-    data = {'email': email, 'password': password}
-    response = requests.post(f'{DB_SERVICE_URL}/authenticate', json=data)
-    return response.json().get('user')
+    with psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME
+    ) as connection:
+        cursor = connection.cursor()
+        cursor.execute(
+            'SELECT * FROM users WHERE email=%s', (email,))
+        user = cursor.fetchone()
+
+        if user and hashpw(password.encode('utf-8'), user[4].encode('utf-8')) == user[4].encode('utf-8'):
+            # Passwords match
+            return user
+        else:
+            # Invalid credentials
+            return None
+
 
 # Function to add a new user to the database
 def add_user(first_name, last_name, email, password):
-    data = {'first_name': first_name, 'last_name': last_name,
-            'email': email, 'password': password}
-    response = requests.post(f'{DB_SERVICE_URL}/add_user', json=data)
-    return response
+    # Hash the password before storing it
+    hashed_password = hashpw(password.encode(
+        'utf-8'), gensalt()).decode('utf-8')
+
+    with psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME
+    ) as connection:
+        cursor = connection.cursor()
+        cursor.execute(
+            'INSERT INTO users (first_name, last_name, email, password) VALUES (%s, %s, %s, %s)',
+            (first_name, last_name, email, hashed_password)
+        )
+        connection.commit()
+
 
 # Function to get user_id based on first name, last name, and email
 def get_user_id(first_name, last_name, email):
-    data = {'first_name': first_name, 'last_name': last_name, 'email': email}
-    response = requests.post(f'{DB_SERVICE_URL}/get_user_id', json=data)
-    return response.json().get('user_id', None)
+    with psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME
+    ) as connection:
+        cursor = connection.cursor()
+        cursor.execute('''
+            SELECT id FROM users
+            WHERE first_name = %s AND last_name = %s AND email = %s
+        ''', (first_name, last_name, email))
+
+        user_id = cursor.fetchone()
+
+    return user_id[0] if user_id else None
 
 
 # Function to generate a random session ID
 def generate_session_id():
     return str(uuid.uuid4())
 
+
 # Function to add a new session to the sessions table
 def add_session(user_id, session_id):
-    data = {'user_id': user_id, 'session_id': session_id}
-    requests.post(f'{DB_SERVICE_URL}/add_session', json=data)
-    
-    
+    with psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME
+    ) as connection:
+        cursor = connection.cursor()
+        cursor.execute(
+            'INSERT INTO sessions (user_id, session_id) VALUES (%s, %s)',
+            (user_id, session_id)
+        )
+        connection.commit()
+
+
 # Function to subscribe the user based on the chosen tier
 def subscribe(user_id, subscription_choice):
     if subscription_choice == 'ad-tier':
         # For Ad-tier, set paidSubscriber to NO and amount to 0
-        data = {'userId': user_id, 'paidSubscriber': 'NO', 'amount': 0}
-        requests.post(f'{DB_SERVICE_URL}/add_subscription', json=data)
+        with psycopg2.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_NAME
+        ) as connection:
+            cursor = connection.cursor()
+            cursor.execute(
+                'INSERT INTO subscriptions (user_id, paidSubscriber, amount) VALUES (%s, %s, %s)',
+                (user_id, 'NO', 0)
+            )
+            connection.commit()
         # Redirect to login page after successful signup for ad-tier
         return redirect(url_for('login'))
 
     elif subscription_choice == 'paid-tier':
-        # For Paid-tier, set paidSubscriber to YES and amount to 5, and store in MongoDB
-        data = {'userId': user_id, 'paidSubscriber': 'PENDING',
-                'amount': 5, 'timestamp': datetime.now()}
-        subscription_collection.insert_one(data)
-        
-        # Send a POST request to /subscribe with the user_id in the JSON payload
-        requests.post('http://127.0.0.1:5002/subscribe', json={'user_id': user_id})
+        # For Paid-tier, set paidSubscriber to YES and amount to 5
+        with psycopg2.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_NAME
+        ) as connection:
+            cursor = connection.cursor()
+            cursor.execute(
+                'INSERT INTO subscriptions (user_id, paidSubscriber, amount) VALUES (%s, %s, %s)',
+                (user_id, 'PENDING', 5)
+            )
+            connection.commit()
         # Redirect to subscribe page after successful signup for paid-tier
         return redirect('http://127.0.0.1:5002/subscribe')
 
@@ -69,10 +184,11 @@ def subscribe(user_id, subscription_choice):
         return jsonify({'error': 'Invalid subscription choice'})
 
 
-#load homepage
+# Load homepage
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -86,9 +202,9 @@ def login():
             # Generate a session ID and add the session to the sessions table
             session_id = generate_session_id()
             add_session(user[0], session_id)
-            
+
             # Redirect to the home page
-            return redirect('http://127.0.0.1:5001/home')
+            return redirect('http://127.0.0.1:5001/ad-tier')
 
         return render_template('login.html', error="Invalid email or password")
 
@@ -109,35 +225,19 @@ def signup():
             return render_template('signup.html', error="Passwords do not match")
 
         # Add user to the database without assignment
-        new_user = add_user(first_name, last_name, email, password)
-        
-        if new_user != None:
-            # Get user_id after adding the user
-            user_id = get_user_id(first_name, last_name, email)
+        add_user(first_name, last_name, email, password)
 
-            # Subscribe the user based on the chosen tier
-            subscription_choice = request.form.get('subscription')
-            print(subscription_choice)
-            # subscribe(user_id, subscription_choice)
-            if subscription_choice == 'ad-tier':
-                # For Ad-tier, set paidSubscriber to NO and amount to 0
-                data = {'userId': user_id, 'paidSubscriber': 'NO', 'amount': 0}
-                requests.post(f'{DB_SERVICE_URL}/add_subscription', json=data)
-                # Redirect to login page after successful signup for ad-tier
-                return redirect(url_for('login'))
+        # Get user_id after adding the user
+        user_id = get_user_id(first_name, last_name, email)
 
-            elif subscription_choice == 'paid-tier':
-                # For Paid-tier, set paidSubscriber to YES and amount to 5, and store in MongoDB
-                data = {'userId': user_id, 'paidSubscriber': 'PENDING',
-                        'amount': 5, 'timestamp': datetime.now()}
-                subscription_collection.insert_one(data)
-                
-                # Send a POST request to /subscribe with the user_id in the JSON payload
-                requests.post('http://127.0.0.1:5002/subscribe', json={'user_id': user_id})
-                # Redirect to subscribe page after successful signup for paid-tier
-                return redirect('http://127.0.0.1:5002/subscribe')
+        # Subscribe the user based on the chosen tier
+        subscription_choice = request.form.get('subscription')
+        print(subscription_choice)
+        subscribe(user_id, subscription_choice)
 
     return render_template('signup.html')
 
+
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0",debug=False,port= 5000) #will listen to all ports on production
+    create_tables()  # creates table first
+    app.run(host="0.0.0.0", debug=False, port=5000)  # will listen to all ports on production
